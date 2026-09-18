cb823f699d5ba91bcfb537b3c6a6f5303811275f
Maria Mathai <121696991+mariamathai29@users.noreply.github.com>
2026-09-17 22:16:39 -0400

    8 bit counter
    Refactor module inputs and outputs, implement counter logic.

--- src/project.v
+++ src/project.v
@@ -3,25 +3,64 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+    // input and output enables via ui_in 0,1
+    wire       load = ui_in[0];
+    wire       oe   = ui_in[1];
+    wire [7:0] count;
+
+    counter counter_inst (
+        .load    (load),
+        .rst     (~rst_n),
+        .data_in (uio_in),
+        .clk     (clk),
+        .count   (count)
+    );
+
+    assign uo_out = count; // visibility for testing, even when uio_oe is not set 
+
+    // implement tri-state by toggling uio output enable, rather than using hi Z
+    assign uio_out = count;
+    assign uio_oe  = {8{oe}};
+
+    wire _unused = &{ena, ui_in[7:2], 1'b0};
+
+endmodule
+
+module counter (
+    input  wire       load,
+    input  wire       rst,
+    input  wire [7:0] data_in,
+    input  wire       clk,
+    output wire [7:0] count
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    logic [7:0] count_r;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    always_ff @(posedge clk or posedge rst) begin // update ff on rising clock edge or reset detected
+        if (rst) begin
+            count_r <= 8'd0; // set to zero
+        end else begin
+            if (load) begin
+                count_r <= data_in; // sync data load
+            end else begin
+                count_r <= count_r + 8'd1; // otherwise continue to count up
+            end
+        end
+    end
+
+    assign count = count_r;
 
 endmodule
